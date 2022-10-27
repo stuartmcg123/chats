b6c6f5c0a300a5d0f8c8eaf2b68162e294946a09
Stuart McGillivray <stuart.mcg@hotmail.co.uk>
2022-10-27 13:41:06 +0100

    making pwa

--- stui/src/app/app.module.ts
+++ stui/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthConfigModule } from './auth/auth-config.module';
 import { TokenInterceptor } from './token.interceptor';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,13 @@ import { TokenInterceptor } from './token.interceptor';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AuthConfigModule
+    AuthConfigModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
